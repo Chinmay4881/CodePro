bb8a591874615a007c6dd4a3bac12c2e1b3c1383
Chinmay4881 <kulkarnichinmay029@gmail.com>
2025-04-10 19:41:03 +0530

    Create Lc3452,sum_of_good_num.cpp
    Return the sum of all the good elements in the array.

---
+++ Lc3452,sum_of_good_num.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int sumOfGoodNumbers(vector<int>& nums, int k) {
+        int n = nums.size();
+        long long sum = 0;
+
+        for (int i = 0; i < n; ++i) {
+            bool isGood = true;
+
+            if (i - k >= 0 && nums[i] <= nums[i - k]) {
+                isGood = false;
+            }
+
+            if (i + k < n && nums[i] <= nums[i + k]) {
+                isGood = false;
+            }
+
+            if (isGood) {
+                sum += nums[i];
+            }
+        }
+
+        return sum;
+    }
+};
