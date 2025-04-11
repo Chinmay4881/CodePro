b6a6705c4aa2511a8f8127c97817f4948afe505e
Chinmay4881 <kulkarnichinmay029@gmail.com>
2025-04-11 21:51:58 +0530

    Create Funprint.cpp
    Pattern tech

---
+++ Funprint.cpp
@@ -0,0 +1,37 @@
+void funprint(int num){
+
+for(int i=0;i<num;i++){
+
+for(int j=0;j<(2*num-2*1-2)/2;j++){
+
+cout<<" ";
+
+}for(int j=0;j<num+2*i-2;j++) {
+
+cout<<"*";
+
+}
+
+for(int j=0;j<(2*num-21-2/2);j++){
+
+cout<<" ";
+
+}
+
+cout<<endl;
+
+I
+
+}
+
+}
+
+int main(){
+
+int num;
+
+num=3
+
+funprint(num);
+
+}
