8d57203bbd2e5e852fa1e2e2e49651a0c86561c3
Chinmay4881 <kulkarnichinmay029@gmail.com>
2025-08-21 22:43:13 +0530

    Create LC448. Find All Numbers Disappeared in an Array.cpp
    by set logic

---
+++ LC448. Find All Numbers Disappeared in an Array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        vector<int>result;
+        unordered_set<int>seto(nums.begin(),nums.end());
+        for(int i=1;i<=nums.size();i++){
+            if(seto.find(i)==seto.end()){
+                result.push_back(i);
+            }
+        }
+        return result;
+    }
+};
