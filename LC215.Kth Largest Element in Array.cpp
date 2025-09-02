28d3dbe49e02456e30596d1ff82e86609b3d0499
Chinmay4881 <kulkarnichinmay029@gmail.com>
2025-09-02 22:44:32 +0530

    Create LC215.Kth Largest Element in Array.cpp
    by sorting method

---
+++ LC215.Kth Largest Element in Array.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end(),greater<int>());
+        for(int i=0;i<nums.size();i++){
+            return nums[k-1];
+        }
+        return -1;
+    }
+};
